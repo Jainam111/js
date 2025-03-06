112081f1471cee2ab3cecedeb1c2f5d1827ed84e
Jainam012 <bhavsarjainam33@gmail.com>
2025-03-06 18:33:48 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,4 +21,45 @@ let booleanIsLoggedIn = Boolean(isLoggenIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+// ******************* Operations **********************
+
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  // 2^2
+// console.log(2**3);  // 2^3
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " jainam"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// as soon as js detects string it does string concatination otherwise 
+// it does normal operation of adition
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
