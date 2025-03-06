ec4f56afd2ce3608a64c8a61c683c6c9092c3074
Jainam012 <bhavsarjainam33@gmail.com>
2025-03-06 17:47:30 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEMail = "bhavsarjainam33@gmail.com"
+let accountPassword = "12333"
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = 2
+accountEMail = "jainam111@jai.com"
+accountCity = "Delhi" 
+console.log(accountId);
+
+console.table([accountId, accountEMail, accountCity, accountPassword, accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // Treat all JS code as new version
+
+// alert(3 + 3) //we are using node.js not browser so this does not work
+
+console.log(3 
+    +  
+    3); // code readability should ben high this is not a good practice
+
+console.log("Jainam");
+
+let name = "Jainam"
+let age = 19
+let isLoggenIn = false
+
+// number => 2 to power 53
+// bigInt
+// string
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);    // object
+
+
