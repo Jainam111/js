2554f1ec4b0a013ca08c1cf15c53714f02bdff05
Jainam012 <bhavsarjainam33@gmail.com>
2025-03-06 19:23:34 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 = 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);  
+// console.log(null == 0);
+// console.log(null >= 0);     
+// comparisons (> , < , <=) convert null to 0 and == does not 
+
+// console.log(undefined == 0);     
+
+// Strict check (===)
+
+// console.log("2" === 2); 
+// does not do conversion and also checks datatype 
