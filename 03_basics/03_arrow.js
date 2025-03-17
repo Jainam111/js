2ddcf305743859f4ea6ecd66487c24b3f8d34844
Jainam Bhavsar <jainam.bhavsar24@spit.ac.in>
2025-03-17 18:38:07 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,55 @@
+// in the below code -> the this.username refers to the username present in that scope
+// this. is important because there might be another variable with same name outside 
+// soo this. refers to the current context
+const user = {
+    username: "jainam",
+    price: 999,
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        // console.log(this);
+        
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);  // empty 
+
+
+// in the below code if we try to apply the concept we learned in the above code it wont 
+// work because it only works with objects
+// function chai() {
+//     let username = "jainam"
+//     console.log(this.username)
+// }
+
+
+// given below is an arrow function it's declaed as it is shown 
+// syntax :- () => {}
+const chai = () => {
+    let username = "jainam"
+    console.log(this.username)
+}
+
+// chai()
+
+// below given is normal arrow function
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// below given is an implicit return arrow function . we have to write return keyword if there are curly braces
+// explicit return is when we use return keyword
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+// const addTwo = (num1, num2) => {username: "jainam"} // this will give error as there is object and "()" are not used
+// const addTwo = (num1, num2) => ({username: "jainam"}) // this wont give error
+
+
+// console.log(addTwo(2, 4));
