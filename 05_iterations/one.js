c072e619f328e5ae6c0993fda7abc92394b922ee
Jainam Bhavsar <jainam.bhavsar24@spit.ac.in>
2025-03-21 17:16:42 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,46 @@
+// for
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i
+//     console.log(element);   
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Table of ${i}`);
+    
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value ${j} outer ${i}`);
+//         console.log(`${i} * ${j} = ${i*j}`);
+        
+//     }
+// }
+
+
+
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+// }
+
+
+// break and continue
+
+// for (let index = 0; index < 10; index++) {
+//     if (index == 5) {
+//         console.log(`5 detected`);
+//         break;  
+//     }
+//     console.log(`index is ${index}`);
+// }
+
+
+for (let index = 0; index < 10; index++) {
+    if (index == 5) {
+        console.log(`5 detected`);
+        continue;
+    }
+    console.log(`index is ${index}`);
+}
